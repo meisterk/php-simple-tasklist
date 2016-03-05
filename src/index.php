a498726feb2315960c361dc6e2aced0ff2f2a66a
Franz Kohnle <franz@kohnlehome.de>
2016-03-05 21:12:07 +0100

    created index.php

---
+++ src/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    </head>
+    <body>
+        <h1>Simple PHP Tasklist</h1>
+        <?php
+            echo "TEST";
+        ?>
+    </body>        
+</html>
+
+
