26d04b29b716894448390d9bbbfd0ecc07c5952f
Franz Kohnle <franz@kohnlehome.de>
2016-03-20 22:37:09 +0100

    CRUD-Function added to Database.php and MySQLDatabase.php

--- src/Database.php
+++ src/Database.php
@@ -2,5 +2,16 @@
 
 interface Database {
 
+    // C R U D
+    // CREATE
+    public function createTask(Task $task);
+
+    // READ
     public function getTaskList();
+
+    // UPDATE
+    public function updateTask(Task $task);
+
+    // DELETE
+    public function deleteTask(Task $task);
 }
--- src/MySQLDatabase.php
+++ src/MySQLDatabase.php
@@ -5,27 +5,73 @@ require 'Task.php';
 
 class MySQLDatabase implements Database {
 
-    public function getTaskList() {
+    private $db;
+
+    public function __construct() {
         // create connection to database
         $user = 'root';
-        $password = '';
-        $db = new PDO('mysql:host=localhost;dbname=tasklist', $user, $password);
+        $password = 'mysql';
+        $this->db = new PDO('mysql:host=localhost;dbname=tasklist', $user, $password);
+    }
+
+    public function createTask(Task $task) {
+        // The id of the task is ignored
+        // id should be set by MySQL per auto_increment
+        $stmt = $this->db->prepare(
+                "INSERT INTO task (timestamp, text)" .
+                " VALUES (:timestamp, :text)");
+
+        $stmt->bindParam(':timestamp', $timestamp);
+        $stmt->bindParam(':text', $text);
+
+        $timestamp = $task->getTimestamp();
+        $text = $task->getText();
+        $stmt->execute();
+    }
+
+    public function getTaskList() {
 
         // the table is saved in an array
         $tasklist = array();
-        
+
         // read data from table
         $sql = 'SELECT * FROM task';
-        foreach ($db->query($sql) as $zeile) {
+        foreach ($this->db->query($sql) as $zeile) {
             $id = $zeile['id'];
             $timestamp = $zeile['timestamp'];
             $text = $zeile['text'];
             $task = new Task($id, $timestamp, $text);
             array_push($tasklist, $task);
         }
-        
+
         // return array of Task-instances
         return $tasklist;
+    }
+   
+    public function updateTask(Task $task) {
+        $sql = "UPDATE task SET timestamp=:timestamp, text=:text WHERE id=:id";
+        $stmt = $this->db->prepare($sql);
+
+        $stmt->bindParam(':timestamp', $timestamp);
+        $stmt->bindParam(':text', $text);
+        $stmt->bindParam(':id', $id);
+
+        $timestamp = $task->getTimestamp();
+        $text = $task->getText();
+        $id = $task->getId();
+
+        $stmt->execute();
+    }
+
+    public function deleteTask(Task $task) {
+        $sql = "DELETE FROM task WHERE id=:id";
+        $stmt = $this->db->prepare($sql);
+
+        $stmt->bindParam(':id', $id);
+
+        $id = $task->getId();
+
+        $stmt->execute();
     }
 
 }
